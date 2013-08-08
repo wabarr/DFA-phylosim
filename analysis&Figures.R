ac80b5dad62644587e43a1cad00f0c9997075c60
wabarr <wabarr@gmail.com>
2013-08-08 14:34:41 -0500

    changed fileinput and shortform names

--- analysis&Figures.R
+++ analysis&Figures.R
@@ -12,13 +12,15 @@ nice<-function(x,places=2){round(x,places)}
 theme_set(theme_bw(20) + theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank()))
 
 setwd("~/Dropbox/WAB Dissertation/Chapter 2 - Methods/")
-#inputFiles<-list.files(path=".",pattern="RANDOMIZED")
-inputFiles<-"BrownianMotionSimResults_fixedS_highR_DontCorrectBodySize_CROSSVALIDATED_RANDOMIZEDHABITATS.txt"
+#inputFiles<-list.files(path=".",pattern="RANDOMIZEDHABITATS")
+inputFiles<-"BrownianMotionSimResults_fixedS_lowR_DontCorrectBodySize_RANDOMIZEDHABITATS.txt"            
+
 rez<-lapply(inputFiles,FUN=function(x){read.table(x,header=T,sep="\t")})
 
 #parse the file names to add a column called simGroup
 lapply(1:length(rez),FUN=function(x){
   rez[[x]]$simGroup<<-rep(gsub(pattern=".txt",replacement="",paste0(str_split(string=inputFiles[x],pattern="_")[[1]][3:4],collapse="-")),nrow(rez[[x]]))
+  rez[[x]]$isCrossvalidated<<-length(grep("CROSSVALIDATED",paste0(str_split(string=inputFiles[x],pattern="_")[[1]])))>0
 })
 
 rez<-do.call(rbind,rez)
@@ -61,7 +63,7 @@ rez$sigsHolmes<-(rez$HolmBonnferroni<.05)
 rez$sigFDR<-(rez$fdrP<.05)
 
 #create a shortForm dataframe that includes only columns with apply to a single dfa subset
-shortFormVarnames<-c("dfaID_simGroup","dfaSuccessRate","nvars","simGroup")
+shortFormVarnames<-c("dfaID_simGroup","dfaSuccessRate","nvars","simGroup","wilkesLambda","PillaiPval","HotellingPval","RoyPval","isCrossvalidated")
 shortForm<-ddply(rez[,shortFormVarnames], "dfaID_simGroup", .fun=function(x) x[1,])
 
 splitSimGroup<-str_split(shortForm$simGroup[1:2],"-")
@@ -71,67 +73,73 @@ levels(shortForm$BM_correlation)<-c("r = 0 no phylo","r = 0 phylo","low r","high
 
 shortForm$BMCorrectionOrNot<-factor(unlist(lapply(str_split(shortForm$simGroup,"-"),FUN=function(x) x[2])))
 levels(shortForm$BMCorrectionOrNot)<-c("Size Corrected","Raw Data")
-shortForm$sCategory<-cut(shortForm$averageS,3)
-levels(shortForm$sCategory)<-c("low S","medium S","high S")
-shortForm$wilkes<-tapply(rez$wilkes,rez$dfaID_simGroup,FUN=unique)
-shortForm$pillai<-tapply(rez$Pillai,rez$dfaID_simGroup,FUN=unique)
-shortForm$hotelling<-tapply(rez$Hotelling,rez$dfaID_simGroup,FUN=unique)
-shortForm$roy<-tapply(rez$Roy,rez$dfaID_simGroup,FUN=unique)
-annotateDF<-ddply(rez,"binnedNvars",.fun=function(x){paste("Mean = ",round(mean(x$dfaSuccessRate),2)*100,"%",sep="")})
-annotateDF2<-ddply(rez,"binnedNvars",.fun=function(x){paste("95th percentile = ",round(quantile(x$dfaSuccessRate,.95),2)*100,"%",sep="")})
+#shortForm$sCategory<-cut(shortForm$averageS,3)
+#levels(shortForm$sCategory)<-c("low S","medium S","high S")
+#shortForm$wilkes<-tapply(rez$wilkes,rez$dfaID_simGroup,FUN=unique)
+#shortForm$pillai<-tapply(rez$Pillai,rez$dfaID_simGroup,FUN=unique)
+#shortForm$hotelling<-tapply(rez$Hotelling,rez$dfaID_simGroup,FUN=unique)
+#shortForm$roy<-tapply(rez$Roy,rez$dfaID_simGroup,FUN=unique)
+#annotateDF<-ddply(rez,"binnedNvars",.fun=function(x){paste("Mean = ",round(mean(x$dfaSuccessRate),2)*100,"%",sep="")})
+#annotateDF2<-ddply(rez,"binnedNvars",.fun=function(x){paste("95th percentile = ",round(quantile(x$dfaSuccessRate,.95),2)*100,"%",sep="")})
 
+####################################PLOTS################################
+####################################PLOTS################################
+####################################PLOTS################################
+# boxplot_SuccessByR_all<-qplot(nvars,dfaSuccessRate,data=shortForm,geom="boxplot",group=interaction(nvars,BMCorrectionOrNot),xlab="# of characters",ylab="Classification Success Rate",fill=BMCorrectionOrNot,facets=~BM_correlation,outlier.size=0) + 
+#   scale_y_continuous(labels=percent) + 
+#   scale_fill_grey(start = 0.9, end = 0.5) + 
+#   labs(fill="Size Correction") + 
+#   guides(fill = guide_legend(keywidth = 1, keyheight = 3)) + 
+#   theme_bw(20) + 
+#   theme(legend.position="bottom",panel.grid.minor=element_blank(),panel.grid.major=element_line(size=.7))
+# 
+# 
+# boxplot_SuccessByR<-qplot(nvars,dfaSuccessRate,data=subset(shortForm, wilkes>.05),geom="boxplot",group=interaction(nvars,BMCorrectionOrNot),xlab="# of characters",ylab="Classification Success Rate",fill=BMCorrectionOrNot,facets=~BM_correlation,outlier.size=0) + 
+#   scale_y_continuous(labels=percent) + 
+#   scale_fill_grey(start = 0.9, end = 0.5) + 
+#   labs(fill="Size Correction") + 
+#   guides(fill = guide_legend(keywidth = 1, keyheight = 3)) + 
+#   theme_bw(20) + 
+#   theme(legend.position="bottom",panel.grid.minor=element_blank(),panel.grid.major=element_line(size=.7))
+# #ggsave("~/Dropbox/WAB Dissertation/Chapter 2 - Methods/boxplotsSuccessByR.pdf",boxplot_SuccessByR,height=9.15,width=14,units="in")
+# 
+# 
+# 
+# densityDFA_all<-qplot(x=dfaSuccessRate*100,data=shortForm,lty=BM_correlation,geom="density",alpha=I(.35),fill=BM_correlation, facets=BMCorrectionOrNot~.,main="Fig 2A - All DFAs",xlab="Success Rate") + 
+#   scale_linetype_manual(values=c("dotted", "solid","longdash","dotdash")) +
+#   scale_x_continuous(limits=c(30,90)) + 
+#   theme(legend.title = element_blank())
+# 
+# densityDFA<-qplot(x=dfaSuccessRate*100,data=subset(shortForm,wilkes<.05),lty=BM_correlation,geom="density",alpha=I(.35),fill=BM_correlation, facets=BMCorrectionOrNot~.,main="Fig 2B - Only Significant DFAs",xlab="Success Rate") + 
+#   scale_linetype_manual(values=c("dotted", "solid","longdash","dotdash")) +
+#   scale_x_continuous(limits=c(30,90)) + 
+#   theme(legend.title = element_blank())
+# pdf(file="~/Dropbox/WAB Dissertation/Chapter 2 - Methods/densityDFA.pdf",width=14,height=11.15)
+# grid.arrange(densityDFA_all,densityDFA,ncol=1)
+# dev.off()
 
-boxplot_SuccessByR_all<-qplot(nvars,dfaSuccessRate,data=shortForm,geom="boxplot",group=interaction(nvars,BMCorrectionOrNot),xlab="# of characters",ylab="Classification Success Rate",fill=BMCorrectionOrNot,facets=~BM_correlation,outlier.size=0) + 
-  scale_y_continuous(labels=percent) + 
-  scale_fill_grey(start = 0.9, end = 0.5) + 
-  labs(fill="Size Correction") + 
-  guides(fill = guide_legend(keywidth = 1, keyheight = 3)) + 
-  theme_bw(20) + 
-  theme(legend.position="bottom",panel.grid.minor=element_blank(),panel.grid.major=element_line(size=.7))
-
-
-boxplot_SuccessByR<-qplot(nvars,dfaSuccessRate,data=subset(shortForm, wilkes>.05),geom="boxplot",group=interaction(nvars,BMCorrectionOrNot),xlab="# of characters",ylab="Classification Success Rate",fill=BMCorrectionOrNot,facets=~BM_correlation,outlier.size=0) + 
-  scale_y_continuous(labels=percent) + 
-  scale_fill_grey(start = 0.9, end = 0.5) + 
-  labs(fill="Size Correction") + 
-  guides(fill = guide_legend(keywidth = 1, keyheight = 3)) + 
-  theme_bw(20) + 
-  theme(legend.position="bottom",panel.grid.minor=element_blank(),panel.grid.major=element_line(size=.7))
-#ggsave("~/Dropbox/WAB Dissertation/Chapter 2 - Methods/boxplotsSuccessByR.pdf",boxplot_SuccessByR,height=9.15,width=14,units="in")
-
-
-
-densityDFA_all<-qplot(x=dfaSuccessRate*100,data=shortForm,lty=BM_correlation,geom="density",alpha=I(.35),fill=BM_correlation, facets=BMCorrectionOrNot~.,main="Fig 2A - All DFAs",xlab="Success Rate") + 
-  scale_linetype_manual(values=c("dotted", "solid","longdash","dotdash")) +
-  scale_x_continuous(limits=c(30,90)) + 
-  theme(legend.title = element_blank())
-
-densityDFA<-qplot(x=dfaSuccessRate*100,data=subset(shortForm,wilkes<.05),lty=BM_correlation,geom="density",alpha=I(.35),fill=BM_correlation, facets=BMCorrectionOrNot~.,main="Fig 2B - Only Significant DFAs",xlab="Success Rate") + 
-  scale_linetype_manual(values=c("dotted", "solid","longdash","dotdash")) +
-  scale_x_continuous(limits=c(30,90)) + 
-  theme(legend.title = element_blank())
-pdf(file="~/Dropbox/WAB Dissertation/Chapter 2 - Methods/densityDFA.pdf",width=14,height=11.15)
-grid.arrange(densityDFA_all,densityDFA,ncol=1)
-dev.off()
-
-
+####################################ENDPLOTS################################
+####################################ENDPLOTS################################
+####################################ENDPLOTS################################
 
 simGroupSummaries_all<-ddply(shortForm,"simGroup",.fun=function(x){
   data.frame(
     BMCorrectionOrNot=unique(x$BMCorrectionOrNot),
+    isCrossvalidated=unique(x$isCrossvalidated),
     BM_correlation=unique(x$BM_correlation),
-    proportionOfDFAsSignificant=sum(x$wilkes<.05)/nrow(x),
+    proportionOfDFAsSignificant=sum(x$wilkesLambda<.05)/nrow(x),
     meanDFASuccessRate=mean(x$dfaSuccessRate*100,na.rm=TRUE),
     sdDFASuccessRate=sd(x$dfaSuccessRate*100,na.rm=TRUE)
   )
 })
 
 
-simGroupSummaries<-ddply(subset(shortForm,wilkes<.05),"simGroup",.fun=function(x){
+simGroupSummaries<-ddply(subset(shortForm,wilkesLambda<.05),"simGroup",.fun=function(x){
   data.frame(
           BMCorrectionOrNot=unique(x$BMCorrectionOrNot),
           BM_correlation=unique(x$BM_correlation),
-          proportionOfDFAsSignificant=sum(x$wilkes<.05)/nrow(x),
+          isCrossvalidated=unique(x$isCrossvalidated),
+          proportionOfDFAsSignificant=sum(x$wilkesLambda<.05)/nrow(x),
           meanDFASuccessRate=mean(x$dfaSuccessRate*100,na.rm=TRUE),
           sdDFASuccessRate=sd(x$dfaSuccessRate*100,na.rm=TRUE)
           )
@@ -151,7 +159,7 @@ simGroupSummaries_all$countSigPGLS<-countSigPGLS
 simGroupSummaries_all$countSigPGLSBonferroni<-countSigPGLSBonferroni
 simGroupSummaries_all$countSigPGLSHolm<-countSigPGLSHolm
 simGroupSummaries_all$countSigPGLSfdr<-countSigPGLSfdr
-print(xtable(simGroupSummaries),type="html")
+print(xtable(simGroupSummaries_all),type="html")
 simGroupSummaries_all
 # #count of how many significant chars per subset
 # countTable<-table(tapply(rez$sigs,rez$dfaID,FUN=sum))
@@ -176,5 +184,5 @@ simGroupSummaries_all
 
 #anova(lm(dfaSuccessRate ~ nvars + BM_correlation + BMCorrectionOrNot + nvars:BM_correlation + BM_correlation:BMCorrectionOrNot , data=shortForm))
 
-write.table(x=shortForm,file="~/Dropbox/WAB Dissertation/Chapter 2 - Methods/shortForm.txt",sep="\t",row.names=FALSE)
+#write.table(x=shortForm,file="~/Dropbox/WAB Dissertation/Chapter 2 - Methods/shortForm.txt",sep="\t",row.names=FALSE)
 
