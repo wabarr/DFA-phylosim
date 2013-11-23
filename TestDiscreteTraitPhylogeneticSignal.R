c55c4186ee8bc26259b45d60f2f2d4de52c71e62
Andrew Barr <wabarr@gmail.com>
2013-11-23 09:34:00 -0600

    added addtl models, and renamed variables to be more meaningful

--- TestDiscreteTraitPhylogeneticSignal.R
+++ TestDiscreteTraitPhylogeneticSignal.R
@@ -1,4 +1,3 @@
-
 ##Example of how to test for PhyloSignal in categorical variable
 require(geiger)
 
@@ -24,25 +23,51 @@ myTree<-drop.tip(myTree,myTree$tip.labe[!myTree$tip.labe %in% habitatDF$Fernande
 zeroTree <- transform(myTree,"lambda",0)
 
 
-####with Actual hab - single parameter model
-lambdaActualData<-fitDiscrete(multi2di(myTree),habs, model="ER")
-lambdaNoPhyloSignal<-fitDiscrete(multi2di(zeroTree),habs, model="ER")
-pchisq(2 * (lambdaActualData$opt$lnL - lambdaNoPhyloSignal$opt$lnL),1,lower.tail=FALSE)
+####with Actual hab - Equal Rates Model (ER)
+ERActualTree<-fitDiscrete(multi2di(myTree),habs, model="ER")
+ERStarPhylogeny<-fitDiscrete(multi2di(zeroTree),habs, model="ER")
+pchisq(2 * (ERActualTree$opt$lnL - ERStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
 
-#####with random habs
+#####with random habs  - Equal Rates Model (ER)
 randomHabs <- sample(unique(habs),length(habs),replace=TRUE)
 names(randomHabs) <- names(habs)
-lambdaActualData<-fitDiscrete(multi2di(myTree),randomHabs, model="ER")
-lambdaNoPhyloSignal<-fitDiscrete(multi2di(zeroTree),randomHabs, model="ER")
-pchisq(2 * (lambdaActualData$opt$lnL - lambdaNoPhyloSignal$opt$lnL),1,lower.tail=FALSE)
+ERActualTree<-fitDiscrete(multi2di(myTree),randomHabs, model="ER")
+ERStarPhylogeny<-fitDiscrete(multi2di(zeroTree),randomHabs, model="ER")
+pchisq(2 * (ERActualTree$opt$lnL - ERStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
 
 
-####with Actual hab - symetric model
-ALTlambdaActualData<-fitDiscrete(multi2di(myTree),habs, model="SYM")
-ALTlambdaNoPhyloSignal<-fitDiscrete(multi2di(zeroTree),habs,model="SYM")
-pchisq(2 * (ALTlambdaActualData$opt$lnL - ALTlambdaNoPhyloSignal$opt$lnL),1,lower.tail=FALSE)
+####with Actual hab  - symetric rates model (SYM)
+SYMActualTree<-fitDiscrete(multi2di(myTree),habs, model="SYM")
+SYMStarPhylogeny<-fitDiscrete(multi2di(zeroTree),habs,model="SYM")
+pchisq(2 * (SYMActualTree$opt$lnL - SYMStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
 
-####with Actual hab - meristic model
-ALT2lambdaActualData<-fitDiscrete(multi2di(myTree),habs,model="meristic")
-ALT2lambdaNoPhyloSignal<-fitDiscrete(multi2di(zeroTree),habs,model="meristic")
-pchisq(2 * (ALT2lambdaActualData$opt$lnL - ALT2lambdaNoPhyloSignal$opt$lnL),1,lower.tail=FALSE)
+#####with random habs  - symetric rates model (SYM)
+randomHabs <- sample(unique(habs),length(habs),replace=TRUE)
+names(randomHabs) <- names(habs)
+SYMActualTree<-fitDiscrete(multi2di(myTree),randomHabs, model="SYM")
+SYMStarPhylogeny<-fitDiscrete(multi2di(zeroTree),randomHabs,model="SYM")
+pchisq(2 * (SYMActualTree$opt$lnL - SYMStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
+
+####with Actual hab - all rates different model (ARD)
+ARDActualTree<-fitDiscrete(multi2di(myTree),habs,model="meristic")
+ARDStarPhylogeny<-fitDiscrete(multi2di(zeroTree),habs,model="meristic")
+pchisq(2 * (ARDActualTree$opt$lnL - ARDStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
+
+####with random habs - all rates different model (ARD)
+randomHabs <- sample(unique(habs),length(habs),replace=TRUE)
+names(randomHabs) <- names(habs)
+ARDActualTree<-fitDiscrete(multi2di(myTree),randomHabs,model="meristic")
+ARDStarPhylogeny<-fitDiscrete(multi2di(zeroTree),randomHabs,model="meristic")
+pchisq(2 * (ARDActualTree$opt$lnL - ARDStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
+
+####with Actual hab - meristic model (ordered states)
+MeristicActualTree<-fitDiscrete(multi2di(myTree),habs,model="meristic")
+MeristicStarPhylogeny<-fitDiscrete(multi2di(zeroTree),habs,model="meristic")
+pchisq(2 * (MeristicActualTree$opt$lnL - MeristicStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
+
+#####with random habs  - meristic model (ordered states)
+randomHabs <- sample(unique(habs),length(habs),replace=TRUE)
+names(randomHabs) <- names(habs)
+MeristicActualTree<-fitDiscrete(multi2di(myTree),randomHabs,model="meristic")
+MeristicStarPhylogeny<-fitDiscrete(multi2di(zeroTree),randomHabs,model="meristic")
+pchisq(2 * (MeristicActualTree$opt$lnL - MeristicStarPhylogeny$opt$lnL),1,lower.tail=FALSE)
